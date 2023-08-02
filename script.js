3c40c8a18b1b2b170f461507f9ffcf3d36e6454d
carolspita <carolina.santosbarbosa24@gmail.com>
2023-08-01 22:06:52 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var button = document.getElementById("read_button");
-
-button.addEventListener('click', function() {
-    var card = document.querySelector(".conteudo-quaternario-container-card");
-    card.classList.toggle("active");
-});
