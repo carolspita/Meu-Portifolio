b6620385bc53fbc2e237546bb0070a0fb24b1479
AnaPaula-Barreto <151856816+AnaPaula-Barreto@users.noreply.github.com>
2024-05-16 23:02:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var btn = document.querySelector('#show-or-hiden');
-var container = document.querySelector('.conteudo-projeto-escondido');
+var container = document.querySelector('.container-escondido');
 
 btn.addEventListener('click', function(){
     if(container.style.display === 'block' ){
